3465bd0ee0b29e7ff9bbd194dedc5a194f21d892
Bystrou Ihar <bystrov.igor25@gmail.com>
2025-01-27 12:20:58 +0300

    init: class component progect

---
+++ rs-react-app/src/main.tsx
@@ -0,0 +1,6 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>);
