35a8c8e2b451ca2251e6ec43e53bfa2a3f8879d4
k0nnte <bystrov.igor25@gmail.com>
2025-01-29 15:01:32 +0300

    feat: add top contener, input field, saving to localstorage, search button

---
+++ rs-react-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Top from './top/top';
+
+class App extends React.Component {
+  state = {
+    data: localStorage.getItem('search') || '',
+  };
+
+  updateData = (newData: string) => {
+    const currentData = localStorage.getItem('search');
+    if (newData === currentData) {
+      return;
+    }
+    this.setState({ data: newData });
+    localStorage.setItem('search', newData);
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div className="main">
+        <div className="app">
+          <div className="top">
+            <Top search={this.state.data} onSearch={this.updateData} />
+          </div>
+          <div className="bottom"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ rs-react-app/src/top/top.tsx
@@ -0,0 +1,35 @@
+import React, { createRef } from 'react';
+import './top.css';
+
+interface TopProps {
+  search: string;
+  onSearch: (newData: string) => void;
+}
+
+class Top extends React.Component<TopProps> {
+  inputref = createRef<HTMLInputElement>();
+
+  click = () => {
+    if (this.inputref.current) {
+      this.props.onSearch(this.inputref.current.value.trim());
+    }
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div className="search_vrapper">
+        <input
+          type="text"
+          className="input_search"
+          defaultValue={this.props.search}
+          ref={this.inputref}
+        />
+        <button className="btn_search" onClick={this.click}>
+          Search
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Top;
