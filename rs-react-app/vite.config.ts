b78a6276cd05db9521b83ccd7ec6a8ed6665b8de
k0nnte <bystrov.igor25@gmail.com>
2025-02-07 15:22:18 +0300

    test: add coverage

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -7,5 +7,14 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
+    },
   },
 });
