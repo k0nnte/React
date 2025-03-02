8a3c7dc4b9eb6a6d7134b5ab1ac191ccdf569e23
k0nnte <bystrov.igor25@gmail.com>
2025-03-02 04:23:35 +0300

    feat: add test notFound

--- rs-react-app/app/not-found.tsx
+++ rs-react-app/app/not-found.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '@/src/other/404/NotFound.css';
+import '../src/other/404/NotFound.css';
 
 const NotFound: React.FC = () => {
   return <div className="errorPage">404 Page Not Found</div>;
