745f3042aadd32eae18cd4cd0d2acd81dcde999d
k0nnte <bystrov.igor25@gmail.com>
2025-03-02 11:20:49 +0300

    test: add top test

--- rs-react-app/src/top/top.test.tsx
+++ rs-react-app/src/top/top.test.tsx
@@ -1,9 +1,130 @@
+// import { describe, expect, test, vi } from 'vitest';
+// import { fireEvent, render, screen } from '@testing-library/react';
+// import { useNavigate, useSearchParams } from 'react-router-dom';
+// import { ThemeProvider } from '../other/context/theme';
+// import Top from './top';
+// import { useTheme } from '../other/context/useTheme';
+// const dispatch = vi.fn();
+
+// vi.mock('../other/context/useTheme', () => ({
+//   useTheme: vi.fn().mockReturnValue({ theme: 'white', setTheme: vi.fn() }),
+// }));
+
+// vi.mock('react-redux', () => ({
+//   useDispatch: () => dispatch,
+// }));
+// const dataMock = vi.fn();
+// vi.mock(import('../other/localhook'), async (importOriginal) => {
+//   const actual = await importOriginal();
+//   return {
+//     ...actual,
+//     useLocalStorage: vi.fn().mockReturnValue(['', () => dataMock]),
+//   };
+// });
+
+// vi.mock('react-router-dom', () => ({
+//   useNavigate: vi.fn(),
+//   useSearchParams: vi.fn(),
+// }));
+
+// describe('test top', () => {
+//   test('test click btn no text', () => {
+//     const mockSearchParams = new URLSearchParams();
+//     const mockSetSearchParams = vi.fn();
+//     const mockNavigate = vi.fn();
+//     vi.mocked(useNavigate).mockReturnValue(mockNavigate);
+//     vi.mocked(useSearchParams).mockReturnValue([
+//       mockSearchParams,
+//       mockSetSearchParams,
+//     ]);
+//     render(
+//       <ThemeProvider>
+//         <Top />
+//       </ThemeProvider>
+//     );
+//     const button = screen.getByText('Search');
+//     fireEvent.click(button);
+//     expect(mockNavigate).not.toHaveBeenCalled();
+//     expect(dispatch).not.toHaveBeenCalled();
+//     expect(dataMock).not.toHaveBeenCalled();
+//   });
+//   test('test click btn', () => {
+//     const mockSearchParams = new URLSearchParams();
+//     const mockSetSearchParams = vi.fn();
+//     const mockNavigate = vi.fn();
+//     vi.mocked(useNavigate).mockReturnValue(mockNavigate);
+//     vi.mocked(useSearchParams).mockReturnValue([
+//       mockSearchParams,
+//       mockSetSearchParams,
+//     ]);
+//     render(
+//       <ThemeProvider>
+//         <Top />
+//       </ThemeProvider>
+//     );
+//     const input = screen.getByRole('textbox');
+//     fireEvent.change(input, { target: { value: 'new search text' } });
+//     const button = screen.getByText('Search');
+//     fireEvent.click(button);
+//     expect(mockNavigate).toHaveBeenCalled();
+//     expect(dispatch).toHaveBeenCalled();
+//   });
+//   test('toggle theme white', () => {
+//     const mockSearchParams = new URLSearchParams();
+//     const mockSetSearchParams = vi.fn();
+//     const mockNavigate = vi.fn();
+//     const mocksetTheme = vi.fn();
+//     vi.mocked(useTheme).mockReturnValue({
+//       theme: 'white',
+//       setTheme: mocksetTheme,
+//     });
+//     vi.mocked(useNavigate).mockReturnValue(mockNavigate);
+//     vi.mocked(useSearchParams).mockReturnValue([
+//       mockSearchParams,
+//       mockSetSearchParams,
+//     ]);
+//     render(
+//       <ThemeProvider>
+//         <Top />
+//       </ThemeProvider>
+//     );
+//     const themeSelect = screen.getByRole('combobox');
+//     fireEvent.change(themeSelect, { target: { value: 'dark' } });
+//     expect(mocksetTheme).toHaveBeenCalledWith('dark');
+//   });
+//   test('toggle theme dark', () => {
+//     const mockSearchParams = new URLSearchParams();
+//     const mockSetSearchParams = vi.fn();
+//     const mockNavigate = vi.fn();
+//     const mocksetTheme = vi.fn();
+//     vi.mocked(useTheme).mockReturnValue({
+//       theme: 'dark',
+//       setTheme: mocksetTheme,
+//     });
+//     vi.mocked(useNavigate).mockReturnValue(mockNavigate);
+//     vi.mocked(useSearchParams).mockReturnValue([
+//       mockSearchParams,
+//       mockSetSearchParams,
+//     ]);
+//     render(
+//       <ThemeProvider>
+//         <Top />
+//       </ThemeProvider>
+//     );
+//     const themeSelect = screen.getByRole('combobox');
+//     fireEvent.change(themeSelect, { target: { value: 'white' } });
+//     expect(mocksetTheme).toHaveBeenCalledWith('white');
+//   });
+// });
+
 import { describe, expect, test, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+// import { useSearchParams } from 'react-router-dom';
 import { ThemeProvider } from '../other/context/theme';
 import Top from './top';
 import { useTheme } from '../other/context/useTheme';
+import { useRouter } from 'next/navigation';
+
 const dispatch = vi.fn();
 
 vi.mock('../other/context/useTheme', () => ({
@@ -21,22 +142,33 @@ vi.mock(import('../other/localhook'), async (importOriginal) => {
     useLocalStorage: vi.fn().mockReturnValue(['', () => dataMock]),
   };
 });
-
-vi.mock('react-router-dom', () => ({
-  useNavigate: vi.fn(),
+vi.mock('next/navigation', () => ({
+  useRouter: vi.fn((): { push: (path: string) => void } => ({
+    push: vi.fn(),
+  })),
   useSearchParams: vi.fn(),
+  useParams: vi.fn(),
 }));
 
 describe('test top', () => {
   test('test click btn no text', () => {
-    const mockSearchParams = new URLSearchParams();
-    const mockSetSearchParams = vi.fn();
-    const mockNavigate = vi.fn();
-    vi.mocked(useNavigate).mockReturnValue(mockNavigate);
-    vi.mocked(useSearchParams).mockReturnValue([
-      mockSearchParams,
-      mockSetSearchParams,
-    ]);
+    // const mockGet = vi.fn().mockReturnValue('1');
+    // const mockHas = vi.fn().mockReturnValue(true);
+    const mockPush = vi.fn();
+    // const mockSearchParams = {
+    //   get: mockGet,
+    //   has: mockHas,
+    //   toString: vi.fn(() => ''),
+    // } as unknown as ReadonlyURLSearchParams;
+    // vi.mocked(useSearchParams).mockReturnValue(mockSearchParams);
+    vi.mocked(useRouter).mockReturnValue({
+      push: mockPush,
+      back: vi.fn(),
+      forward: vi.fn(),
+      refresh: vi.fn(),
+      replace: vi.fn(),
+      prefetch: vi.fn(),
+    });
     render(
       <ThemeProvider>
         <Top />
@@ -44,19 +176,20 @@ describe('test top', () => {
     );
     const button = screen.getByText('Search');
     fireEvent.click(button);
-    expect(mockNavigate).not.toHaveBeenCalled();
+    expect(mockPush).not.toHaveBeenCalled();
     expect(dispatch).not.toHaveBeenCalled();
     expect(dataMock).not.toHaveBeenCalled();
   });
   test('test click btn', () => {
-    const mockSearchParams = new URLSearchParams();
-    const mockSetSearchParams = vi.fn();
-    const mockNavigate = vi.fn();
-    vi.mocked(useNavigate).mockReturnValue(mockNavigate);
-    vi.mocked(useSearchParams).mockReturnValue([
-      mockSearchParams,
-      mockSetSearchParams,
-    ]);
+    const mockPush = vi.fn();
+    vi.mocked(useRouter).mockReturnValue({
+      push: mockPush,
+      back: vi.fn(),
+      forward: vi.fn(),
+      refresh: vi.fn(),
+      replace: vi.fn(),
+      prefetch: vi.fn(),
+    });
     render(
       <ThemeProvider>
         <Top />
@@ -66,23 +199,15 @@ describe('test top', () => {
     fireEvent.change(input, { target: { value: 'new search text' } });
     const button = screen.getByText('Search');
     fireEvent.click(button);
-    expect(mockNavigate).toHaveBeenCalled();
+    expect(mockPush).toHaveBeenCalled();
     expect(dispatch).toHaveBeenCalled();
   });
   test('toggle theme white', () => {
-    const mockSearchParams = new URLSearchParams();
-    const mockSetSearchParams = vi.fn();
-    const mockNavigate = vi.fn();
     const mocksetTheme = vi.fn();
     vi.mocked(useTheme).mockReturnValue({
       theme: 'white',
       setTheme: mocksetTheme,
     });
-    vi.mocked(useNavigate).mockReturnValue(mockNavigate);
-    vi.mocked(useSearchParams).mockReturnValue([
-      mockSearchParams,
-      mockSetSearchParams,
-    ]);
     render(
       <ThemeProvider>
         <Top />
@@ -93,19 +218,11 @@ describe('test top', () => {
     expect(mocksetTheme).toHaveBeenCalledWith('dark');
   });
   test('toggle theme dark', () => {
-    const mockSearchParams = new URLSearchParams();
-    const mockSetSearchParams = vi.fn();
-    const mockNavigate = vi.fn();
     const mocksetTheme = vi.fn();
     vi.mocked(useTheme).mockReturnValue({
       theme: 'dark',
       setTheme: mocksetTheme,
     });
-    vi.mocked(useNavigate).mockReturnValue(mockNavigate);
-    vi.mocked(useSearchParams).mockReturnValue([
-      mockSearchParams,
-      mockSetSearchParams,
-    ]);
     render(
       <ThemeProvider>
         <Top />
